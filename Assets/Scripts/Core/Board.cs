4935f0dc3a6171ea4ba3dff4bbadf3fd6fb0c456
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-10-28 07:09:51 -0700

    add UI

--- Assets/Scripts/Core/Board.cs
+++ Assets/Scripts/Core/Board.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class Board : MonoBehaviour {
 
+	public Transform emptySprite;
+	public int height = 30;
+	public int width = 10;
+	public int header = 8;
+
+	private Transform[,] grid;
+
+	// Called when the script instance is being loaded.
+	void Awake()
+	{
+		grid = new Transform[width, header];
+	}
 	// Use this for initialization
 	void Start () {
-		
+		DrawEmptyCells();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void DrawEmptyCells()
+	{
+		for(int y=0; y < height - header; y++)
+		{
+			for (int x =0 ; x< width; x++){
+				Transform clone;
+				clone = Instantiate(emptySprite, new Vector3(x, y, 0), Quaternion.identity) as Transform;
+				clone.name = "Board Space ( x= "+ x.ToString() + " , y = " + y.ToString() + " )";
+				clone.transform.parent = transform;
+			}
+		}
 	}
 }
