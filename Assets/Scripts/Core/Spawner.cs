e87e0596cb72f75c2d83978d969005896a55578e
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-10-28 21:37:12 -0700

    update

---
+++ Assets/Scripts/Core/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public Shape[] allShapes;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public Shape SpawnShape()
+	{
+		Shape shape = Instantiate(GetRandomShape(), transform.position, Quaternion.identity) as Shape;
+		return shape;
+	}
+
+	Shape GetRandomShape()
+	{
+		int i = Random.Range(0, allShapes.Length);
+		return allShapes[i];
+	}
+}
---
+++ Assets/Scripts/Managers/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	private Board gameBoard;
+	private Spawner spawner;
+	private Shape activeShape;
+
+	// Use this for initialization
+	void Start () {
+		// gameBoard = GameObject.FindWithTag("Board").GetComponent<Board>();
+		// spawner = GameObject.FindWithTag("Spawner").GetComponent<Spawner>();
+
+		gameBoard = GameObject.FindObjectOfType<Board>();
+		spawner = GameObject.FindObjectOfType<Spawner>();
+		
+		if(activeShape == null){
+			activeShape = spawner.SpawnShape();
+		}
+		spawner.transform.position = Vectorf.Round(spawner.transform.position);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
